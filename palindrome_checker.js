efef0d7777b572985eb9ba43f8690014b9a5c6e1
Angaar Uriakhil <79167789+angaar96@users.noreply.github.com>
2021-06-29 15:49:11 +0100

    Delete palindrome_checker.js

--- palindrome_checker.js
+++
@@ -1,5 +0,0 @@
-function palindrome(str) {
-  const anumeric = str.toLowerCase().match(/[a-z0-9]/g);
-  let clone_anumeric = [...anumeric]
-  let clone_reversed = clone_anumeric.reverse()
-  return clone_reversed.join("") === anumeric.join("")
